23e9288630775ed16d6ce20c29a39fe5c1e03951
SeH <seh999@gmail.com>
2012-05-08 13:43:25 -0400

    initial commit

---
+++ src/genifer/java/Genifer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genifer.java;
+
+/**
+ * interface to Genifer clojure
+ * @author me
+ */
+public class Genifer {
+
+    Object eval(String t) {
+        return "Not implemented yet";
+    }
+
+}
---
+++ src/genifer/java/GeniferPanel.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genifer.java;
+
+import java.awt.BorderLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ * Panel representing an interface to Genifer
+ * @author me
+ */
+public class GeniferPanel extends JPanel {
+    
+    public final Genifer genifer;
+    private final JTextField inputText;
+    private final JTextArea outputText;
+
+    public GeniferPanel(Genifer g) {
+        super(new BorderLayout());
+        this.genifer = g;
+        
+        outputText = new JTextArea();
+        add(outputText, BorderLayout.CENTER);
+        
+        inputText = new JTextField();
+        add(inputText, BorderLayout.SOUTH);
+        
+        inputText.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    enter();
+                }
+            }
+            
+        });
+        
+    }
+    public void enter() {
+        String t = inputText.getText();
+        
+        Object result = genifer.eval(t);
+        outputText.append("> " + t + "\n" + result.toString() + "\n\n");
+        
+        inputText.setText("");
+    }
+    
+    public static void main(String[] args) {
+        Genifer g = new Genifer();
+        
+        JFrame window = new JFrame("Genifer");
+        window.add(new GeniferPanel(g));
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+}
