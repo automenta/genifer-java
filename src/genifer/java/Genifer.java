19b3cbcb4c3f5f379a03881a3d9465b5d42e5441
SeH <seh999@gmail.com>
2012-05-14 13:50:34 -0400

    web interface added

--- src/genifer/java/Genifer.java
+++ src/genifer/java/Genifer.java
@@ -4,14 +4,39 @@
  */
 package genifer.java;
 
+import clojure.lang.RT;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * interface to Genifer clojure
  * @author me
  */
 public class Genifer {
 
-    Object eval(String t) {
-        return "Not implemented yet";
+    public Genifer() {
+        try {
+            RT.loadResourceScript("genifer/core.clj");
+        } catch (IOException ex) {
+            Logger.getLogger(Genifer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    Object eval(String t) {       
+        return RT.var("genifer.core", "repl1").invoke(t);
     }
 
+    
+//    public void induce();
+//    public void abduce(String input);
+//    public void backwardChain(String query);
+//    
+//    public void setDebug(int level);
+//    
+//    public Memory getMemory();    
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println(new Genifer().eval("(+ 1 2)"));
+    }
 }
---
+++ src/genifer/java/GeniferWeb.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genifer.java;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ *
+ * @author me
+ */
+public class GeniferWeb extends Spark {
+    
+    final static Genifer genifer = new Genifer();
+    
+    public static Map<String, String> staticpages = new HashMap();
+
+    public static String getLocalFile(File file) throws IOException {
+        int len;
+        char[] chr = new char[4096];
+        final StringBuffer buffer = new StringBuffer();
+        final FileReader reader = new FileReader(file);
+        try {
+            while ((len = reader.read(chr)) > 0) {
+                buffer.append(chr, 0, len);
+            }
+        } finally {
+            reader.close();
+        }
+        return buffer.toString();
+    }
+
+    public static String getStaticPage(String path) throws IOException {
+        if (staticpages.containsKey(path)) {
+            return staticpages.get(path);
+        }
+        String content = getLocalFile(new File("./web/" + path));
+        staticpages.put(path, content);
+        return content;
+    }
+
+
+    public static void main(String[] args) {
+        
+        setPort(9090); // Spark will run on port 9090
+
+
+        get(new Route("/*") {
+
+            @Override
+            public Object handle(Request rqst, Response rspns) {
+                rspns.header("Content-type", "text/html");
+                
+                
+                String url = rqst.pathInfo();
+                String page = "index.html";
+                if (!url.equals("/")) {                
+                    String xpage = url.substring(url.indexOf("/")+1);
+                    if (page.length() > 0)
+                        page = xpage;
+                }
+                
+                System.out.println(rqst.pathInfo());
+                System.out.println(rqst.url() + " -> " + page);
+                
+                try {
+                    return getStaticPage(page);
+                } catch (IOException ex) {
+                    try {
+                        halt(404, getStaticPage("404.html"));
+                    } catch (IOException ex1) {
+                        Logger.getLogger(GeniferWeb.class.getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+                return null;
+            }
+        });
+        Route eval;
+        post(eval = new Route("/eval") {
+
+            @Override
+            public Object handle(Request rqst, Response rspns) {
+                rspns.header("Content-type", "text/html");
+                String c = rqst.queryParams("c").toString();
+                String r = genifer.eval(c).toString();
+                System.out.println(c + " -> " + r);
+                return r;
+            }
+        });
+        get(eval);
+
+    }
+}
+
