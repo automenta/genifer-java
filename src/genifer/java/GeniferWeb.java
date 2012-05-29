5a4e127c7698687c65c8e9a12a3d514aed0dc6cd
YKY <generic.intelligence@gmail.com>
2012-05-29 09:50:29 +0800

    server can now serve images (jpg, png)

--- src/genifer/java/GeniferWeb.java
+++ src/genifer/java/GeniferWeb.java
@@ -4,9 +4,7 @@
  */
 package genifer.java;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -15,20 +13,21 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
+import spark.utils.IOUtils;
 
 /**
  *
  * @author me
  */
 public class GeniferWeb extends Spark {
-    
-    final static Genifer genifer = new Genifer();
-    
-    public static Map<String, String> staticpages = new HashMap();
 
-    public static String getLocalFile(File file) throws IOException {
+    final static Genifer genifer = new Genifer();
+    public static Map<String, Object> staticpages = new HashMap();
+        final static byte[] buf = new byte[1024];
+        final static char[] chr = new char[4096];
+
+    public static String getLocalTextFile(File file) throws IOException {
         int len;
-        char[] chr = new char[4096];
         final StringBuffer buffer = new StringBuffer();
         final FileReader reader = new FileReader(file);
         try {
@@ -41,43 +40,76 @@ public class GeniferWeb extends Spark {
         return buffer.toString();
     }
 
-    public static String getStaticPage(String path) throws IOException {
-        if (staticpages.containsKey(path)) {
-            return staticpages.get(path);
+    public static void getLocalBinaryFile(File file, OutputStream os) throws IOException {
+        FileInputStream in = new FileInputStream(file);
+
+        OutputStreamWriter out = new OutputStreamWriter(os);
+        int count = 0;
+        while ((count = in.read(buf)) >= 0) {
+            os.write(buf, 0, count);
         }
-        String content = getLocalFile(new File("./web/" + path));
+        in.close();
+        out.close();
+
+    }
+
+    public static String getStaticTextFile(String path) throws IOException {
+        if (staticpages.containsKey(path)) {
+            return (String) staticpages.get(path);
+        }
+        String content = getLocalTextFile(new File("./web/" + path));
         staticpages.put(path, content);
         return content;
     }
 
+    public static void getStaticBinaryFile(String path, OutputStream os) throws IOException {
+//        if (staticpages.containsKey(path)) {
+//            return (String)staticpages.get(path);
+//        }
+        getLocalBinaryFile(new File("./web/" + path), os);
+        //staticpages.put(path, content);
+    }
 
     public static void main(String[] args) {
-        
+
         setPort(9090); // Spark will run on port 9090
+       
 
         get(new Route("/*") {
 
             @Override
             public Object handle(Request rqst, Response rspns) {
-                //rspns.header("Content-type", "text/html");
-				//rspns.header("Content-type", "image/jpg");
+                rspns.header("Content-type", "text/html");
+
 
                 String url = rqst.pathInfo();
                 String page = "index.html";
-                if (!url.equals("/")) {                
-                    String xpage = url.substring(url.indexOf("/")+1);
-                    if (page.length() > 0)
+                if (!url.equals("/")) {
+                    String xpage = url.substring(url.indexOf("/") + 1);
+                    if (page.length() > 0) {
                         page = xpage;
+                    }
                 }
 
                 System.out.println(rqst.pathInfo());
                 System.out.println(rqst.url() + " -> " + page);
 
                 try {
-                    return getStaticPage(page);
+                    if (page.endsWith(".jpg")) {
+                        rspns.header("Content-type", "image/jpg");
+
+                        getStaticBinaryFile(page, rspns.raw().getOutputStream());
+                        return null;
+                    } else if (page.endsWith(".png")) {
+                        rspns.header("Content-type", "image/png");
+                        getStaticBinaryFile(page, rspns.raw().getOutputStream());
+                        return null;
+                    } else {
+                        return getStaticTextFile(page);
+                    }
                 } catch (IOException ex) {
                     try {
-                        halt(404, getStaticPage("404.html"));
+                        halt(404, getStaticTextFile("404.html"));
                     } catch (IOException ex1) {
                         Logger.getLogger(GeniferWeb.class.getName()).log(Level.SEVERE, null, ex1);
                     }
@@ -85,22 +117,6 @@ public class GeniferWeb extends Spark {
                 return null;
             }
         });
-
-		Route formularize;
-        post(formularize = new Route("/formularize") {
-
-            @Override
-            public Object handle(Request rqst, Response rspns) {
-                rspns.header("Content-type", "text/html");
-                String c = rqst.queryParams("c").toString();
-				String d = rqst.queryParams("d").toString();
-                String r = genifer.formularize(c, d).toString();
-                // r = r.replaceAll("\n", "<br/>");
-                return r;
-            }
-        });
-        get(formularize);
-
         Route eval;
         post(eval = new Route("/eval") {
 
@@ -109,6 +125,7 @@ public class GeniferWeb extends Spark {
                 rspns.header("Content-type", "text/html");
                 String c = rqst.queryParams("c").toString();
                 String r = genifer.eval(c).toString();
+                r = r.replaceAll("\n", "<br/>");
                 return r;
             }
         });
@@ -116,4 +133,3 @@ public class GeniferWeb extends Spark {
 
     }
 }
-
